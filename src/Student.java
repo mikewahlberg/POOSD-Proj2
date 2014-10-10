41f892a9c90f057ee92e16b9d1365c5a7705e81f
Michael Wahlberg <mike.wahlberg@gmail.com>
2014-10-10 19:46:44 -0400

    Add updated Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,47 @@
 
 public class Student {
-
+	String firstName, lastName;
+	//Date
+	int timeSpent, videosWatched;
+	
+	Student(String first, String last, int time, int videos)
+	{
+		firstName = first;
+		lastName = last;
+		
+		timeSpent = time;
+		videosWatched = videos;
+	}
+	
+	public String getFirst()
+	{
+		return firstName;
+	}
+	
+	public String getLast()
+	{
+		return lastName;
+	}
+	
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	
+	public int getTime()
+	{
+		return timeSpent;
+	}
+	
+	public int getVideos()
+	{
+		return videosWatched;
+	}
+	
+	public String toString()
+	{
+		return getFullName() + "\n" +
+				getTime() + "\n" +
+				getVideos();
+	}
 }
