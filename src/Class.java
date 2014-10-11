44251406ca3623439d263d140cf5cd903f8366dc
Michael Wahlberg <mike.wahlberg@gmail.com>
2014-10-11 01:08:32 -0400

    Modify Class.java

--- src/Class.java
+++ src/Class.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
 public class Class {
-
+	ArrayList<Student> roster = new ArrayList<Student>();
+	String className;
+	
+	Class(String title)
+	{
+		className = title;
+	}
+	
+	public void addStudent(Student tempStud)
+	{
+		roster.add(tempStud);
+		System.out.println(tempStud);
+	}
+	
+	//Add sorting functions.
 }
