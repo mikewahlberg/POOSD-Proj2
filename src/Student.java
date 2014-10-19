a763c52c197420dc1bb66898014cedd6361d09ed
Michael Wahlberg <mike.wahlberg@gmail.com>
2014-10-18 23:01:27 -0400

    Add toString()

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,32 @@
+import java.util.Date;
+
 
 public class Student {
 	String firstName, lastName;
-	//Date
+	Date date;
 	int timeSpent, videosWatched;
 	
-	Student(String first, String last, int time, int videos)
+	Student(String first, String last, int time, int videos, String d)
 	{
 		firstName = first;
 		lastName = last;
-		
+		date = formatDate(d);
 		timeSpent = time;
 		videosWatched = videos;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Date formatDate(String unformatted)
+	{
+		Date d = new Date();
+		
+		String[] split = unformatted.split("/");
+		
+		d.setMonth(Integer.parseInt(split[0]));
+		d.setDate(Integer.parseInt(split[1]));
+		d.setYear(Integer.parseInt(split[2]));
+		
+		return d;
 	}
 	
 	public String getFirst()
@@ -40,8 +56,8 @@ public class Student {
 	
 	public String toString()
 	{
-		return getFullName() + "\n" +
-				getTime() + "\n" +
-				getVideos();
+		return "Name: " + getFullName() + "\n" +
+				"Time: " + getTime() + "\n" +
+				"Number of videos: " + getVideos() + "\n";
 	}
 }
